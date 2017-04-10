70f4827339cc8fb73a78ea086461a1c242bad0df
punk <cryptography@konkuk.ac.kr>
2017-04-10 05:51:33 +0000

    bug fix and new version release

--- pokinator/pokinator.py
+++ pokinator/pokinator.py
@@ -1,21 +1,21 @@
 from random import choice, randint
 from .pokemons import firstgen
-#from .pokemons import firstgen
+
 
 class Pokinator:
     _name1 = firstgen._name1
     _name2 = firstgen._name2
-    
+
     @classmethod
     def generate(self, token_range=9999, delimiter='-', digit=None):
         if not isinstance(token_range, int) or token_range < 0:
             raise RuntimeError('Token range must be a nonnegative integer')
         if not isinstance(delimiter, str):
             raise RuntimeError('Delimiter must be a string')
-        
+
         generated = [choice(self._name1), choice(self._name2)]
-        
-        if digit != None:
+
+        if digit is not None:
             if not isinstance(digit, int) or (digit < 1) or (digit < len(str(token_range))):
                 raise RuntimeError('Digit must be more than 0')
             format_str = '%0.' + str(digit) + 'd'
@@ -23,6 +23,5 @@ class Pokinator:
         else:
             result = randint(0, token_range)
         generated.append(str(result))
-        
-        return delimiter.join(generated)
 
+        return delimiter.join(generated)
--- pokinator/tests.py
+++ pokinator/tests.py
@@ -3,30 +3,32 @@ import unittest
 
 from pokinator import Pokinator
 
+
 class NameGenTest(unittest.TestCase):
     def test_generate(self):
         self.assertTrue(re.match(r'\w+-\w+-\d+', Pokinator.generate()))
-    
+
     def test_not_equal_in_repeated_call(self):
         self.assertNotEqual(Pokinator.generate(), Pokinator.generate())
-    
+
     def test_configurable_range(self):
         self.assertTrue(re.match(r'\w+-\w+-\d$', Pokinator.generate(9)))
-    
+
     def test_wrong_range(self):
         with self.assertRaises(RuntimeError):
             Pokinator.generate('1')
         with self.assertRaises(RuntimeError):
             Pokinator.generate(-1)
-    
+
     def test_wrong_delimiter(self):
         with self.assertRaises(RuntimeError):
             Pokinator.generate(delimiter=1)
-    
+
     def test_wrong_digit(self):
         with self.assertRaises(RuntimeError):
             Pokinator.generate(digit=3)
             Pokinator.generate(token_range=999, digit=4)
+
 
 def run_test():
     print('#1 value test : ' + Pokinator.generate())
