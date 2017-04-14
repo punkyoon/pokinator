78645de7e217b5dcfb3e06b30594d1dbcfcfb9fb
punkyoon <cryptography@konkuk.ac.kr>
2017-04-15 01:52:05 +0900

    added a second gen pokemon names

---
+++ pokinator/pokemons/secondgen.py
@@ -0,0 +1,27 @@
+_name1 = [
+    'Chikorita', 'Bayleef', 'Meganium', 'Cyndaquil', 'Quilava',
+    'Typhlosion', 'Totodile', 'Croconaw', 'Feraligatr', 'Sentret',
+    'Furret', 'Hoothoot', 'Noctowl', 'Ledyba', 'Ledian',
+    'Spinarak', 'Ariados', 'Crobat', 'Chinchou', 'Lanturn',
+    'Pichu', 'Cleffa', 'Igglybuff', 'Togepi', 'Togetic',
+    'Natu', 'Xatu', 'Mareep', 'Flaaffy', 'Ampharos',
+    'MegaAmpharos', 'Bellossom', 'Marill', 'Azumarill', 'Sudowoodo',
+    'Politoed', 'Hoppip', 'Skiploom', 'Jumpluff', 'Aipom',
+    'Sunkern', 'Sunflora', 'Yanma', 'Wooper', 'Quagsire',
+    'Espeon', 'Umbreon', 'Murkrow', 'Slowking', 'Misdreavus',
+    'Unown', 'Wobbuffet', 'Girafarig',
+]
+
+_name2 = [
+    'Pineco', 'Forretress', 'Dunsparce', 'Gligar', 'Steelix',
+    'MegaSteelix', 'Snubbull', 'Granbull', 'Qwilfish', 'Scizor',
+    'MegaScizor', 'Shuckle', 'Heracross', 'MegaHeracross', 'Sneasel',
+    'Teddiursa', 'Ursaring', 'Slugma', 'Magcargo', 'Swinub',
+    'Piloswine', 'Corsola', 'Remoraid', 'Octillery', 'Delibird',
+    'Mantine', 'Skarmory', 'Houndour', 'Houndoom', 'MegaHoundoom',
+    'Kingdra', 'Phanpy', 'Donphan', 'Porygon2', 'Stantler',
+    'Smeargle', 'Tyrogue', 'Hitmontop', 'Smoochum', 'Elekid',
+    'Magby', 'Miltank', 'Blissey', 'Raikou', 'Entei',
+    'Suicune', 'Larvitar', 'Pupitar', 'Tyranitar', 'MegaTyranitar',
+    'Lugia', 'HoOh', 'Celebi',
+]
